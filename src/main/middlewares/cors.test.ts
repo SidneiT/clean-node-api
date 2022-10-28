0e1494ef7067949f902aebed70902759daf0ba53
Sidnei <sidnei.teixeira@outlook.com>
2022-10-28 15:50:35 -0300

    feat: ensure request return json as default content type

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -3,12 +3,12 @@ import app from '../config/app'
 
 describe('Cors Middleware', () => {
   test('Should enable CORS', async () => {
-    app.post('/test_body_parser', (req, res) => {
+    app.get('/test_cors', (req, res) => {
       res.send()
     })
 
     await request(app)
-      .get('/test_body_parser')
+      .get('/test_cors')
       .expect('acccess-control-allow-origin', '*')
       .expect('acccess-control-allow-methods', '*')
       .expect('acccess-control-allow-headers', '*')
