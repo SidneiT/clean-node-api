af9816147b8352b57bf0df1edff69d88346619d1
Sidnei <sidnei.teixeira@outlook.com>
2022-10-28 16:40:47 -0300

    feat: ensure SignUp route return an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,5 +1,11 @@
 import express from 'express'
+
 import middlewares from './middlewares'
+import routes from './routes'
+
 const app = express()
+
+routes(app)
 middlewares(app)
+
 export default app
